fe225944c41a1230ad16ecf164506bad0e576f8b
Elisha Okon <eliscodavid@gmail.com>
2023-01-28 23:12:32 +0100

    Update CTA.jsx

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -11,7 +11,7 @@ export default function Example() {
           <div className="inline-flex rounded-md shadow">
             <a
               href="#"
-              className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+              className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-700 hover:bg-indigo-700"
             >
               Join Our Community
             </a>
