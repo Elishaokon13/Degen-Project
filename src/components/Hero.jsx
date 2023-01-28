a3a1b7eb7120b5849598635a53010056cbcc557b
Elisha Okon <eliscodavid@gmail.com>
2023-01-28 16:07:25 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,17 +5,17 @@ import { MenuIcon, XIcon } from '@heroicons/react/outline'
 import Logo from '../assets/logo.png'
 
 const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
+  { name: 'Home', href: '#' },
+  { name: 'Roadmap', href: '#Roadmap' },
+  { name: 'Tokenomics', href: '#Tokenomics' },
+  { name: 'FAQ', href: '#Faq' },
 ]
 
 export default function Example() {
   return (
     <div className="relative overflow-hidden">
       <Popover as="header" className="relative">
-        <div className="bg-gray-900 pt-6">
+        <div className="bg-blue-700 pt-6">
           <nav
             className="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6"
             aria-label="Global"
@@ -31,7 +31,7 @@ export default function Example() {
                   />
                 </a>
                 <div className="-mr-2 flex items-center md:hidden">
-                  <Popover.Button className="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
+                  <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
                     <span className="sr-only">Open main menu</span>
                     <MenuIcon className="h-6 w-6" aria-hidden="true" />
                   </Popover.Button>
@@ -46,15 +46,10 @@ export default function Example() {
               </div>
             </div>
             <div className="hidden md:flex md:items-center md:space-x-6">
-              <a href="#" className="text-base font-medium text-white hover:text-gray-300">
+              <a href="#" className="text-base font-medium text-white hover:text-blue-300">
                 Log in
               </a>
-              <a
-                href="#"
-                className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700"
-              >
-                Start free trial
-              </a>
+              
             </div>
           </nav>
         </div>
@@ -69,7 +64,7 @@ export default function Example() {
           leaveTo="opacity-0 scale-95"
         >
           <Popover.Panel focus className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top md:hidden">
-            <div className="rounded-lg shadow-md bg-gray-900 ring-1 ring-black ring-opacity-5 overflow-hidden">
+            <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
               <div className="px-5 pt-4 flex items-center justify-between">
                 <div>
                   <img
@@ -79,7 +74,7 @@ export default function Example() {
                   />
                 </div>
                 <div className="-mr-2">
-                  <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-600">
+                  <Popover.Button className="bg-blue rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-600">
                     <span className="sr-only">Close menu</span>
                     <XIcon className="h-6 w-6" aria-hidden="true" />
                   </Popover.Button>
@@ -91,7 +86,7 @@ export default function Example() {
                     <a
                       key={item.name}
                       href={item.href}
-                      className="block px-3 py-2 rounded-md text-base font-medium text-red-600 hover:bg-red-50"
+                      className="block px-3 py-2 rounded-md text-base font-medium text-blue-700 hover:bg-red-50"
                     >
                       {item.name}
                     </a>
@@ -105,14 +100,7 @@ export default function Example() {
                     Start free trial
                   </a>
                 </div>
-                <div className="mt-6 px-5">
-                  <p className="text-center text-base font-medium text-gray-500">
-                    Existing customer?{' '}
-                    <a href="#" className="text-gray-900 hover:underline">
-                      Login
-                    </a>
-                  </p>
-                </div>
+                
               </div>
             </div>
           </Popover.Panel>
@@ -120,7 +108,7 @@ export default function Example() {
       </Popover>
 
       <main>
-        <div className="pt-10 bg-gray-900 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden">
+        <div className="pt-10 bg-white sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden">
           <div className="mx-auto max-w-7xl lg:px-8">
             <div className="lg:grid lg:grid-cols-2 lg:gap-8">
               <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
@@ -128,7 +116,7 @@ export default function Example() {
                  
                   <h1 className="mt-4 text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
                     <span className="block">$ANTA INU</span>
-                    <span className="block text-red-500">a utility based meme project</span>
+                    <span className="block text-blue-700">a utility based meme project</span>
                   </h1>
                   <p className="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
                   Santa INU is with the aim of providing an ecosystem of value to crypto holders. Helping those who lost negatively to the market due to the  negative turn around of Crypto events that occurred in the past months to recover investments with an Apy of 500% through structured  utilities and collaborations. Offering  investors value in rewards, earnings and incentives.
@@ -138,16 +126,16 @@ export default function Example() {
                   </div>
                 </div>
               </div>
-              <div className="mt-12 -mb-16 sm:-mb-48 lg:m-0 lg:relative">
+             {/* <div className="mt-12 -mb-16 sm:-mb-48 lg:m-0 lg:relative">
                 <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
-                  {/* Illustration taken from Lucid Illustrations: https://lucid.pixsellz.io/ */}
+                  Illustration taken from Lucid Illustrations: https://lucid.pixsellz.io/
                   <img
                     className="w-full lg:absolute rounded-md lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
                     src={Background}
                     alt=""
                   />
                 </div>
-              </div>
+              </div> */ }
             </div>
           </div>
         </div>
