1e55fb861b9a33af3c0e17b99c186b1c8c2416c7
Elisha Okon <eliscodavid@gmail.com>
2023-01-28 22:34:48 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Faq from './components/Faq';
-// import Developers from './components/Developers';
+import Tokenomics from './components/Tokenomics';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
 // import Navbar from './components/Navbar';
@@ -13,6 +13,7 @@ function App() {
         {/* <Navbar /> */}
         <Hero />
         <Features />
+        <Tokenomics/>
         <Faq />
         <Footer />
     </div>
