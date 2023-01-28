2a22ea78255edf2eb74f79f5d2bb6748a9d9c6d2
Elisha Okon <eliscodavid@gmail.com>
2023-01-28 22:49:09 +0100

    Create CTA.jsx

---
+++ src/components/CTA.jsx
@@ -0,0 +1,24 @@
+/* This example requires Tailwind CSS v2.0+ */
+export default function Example() {
+  return (
+    <div className="bg-white">
+      <div className="max-w-7xl mx-auto text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          <span className="block">Ready to ape in?</span>
+          <span className="block">To know, is to be Free!.</span>
+        </h2>
+        <div className="mt-8 flex justify-center">
+          <div className="inline-flex rounded-md shadow">
+            <a
+              href="#"
+              className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+            >
+              Join Our Community
+            </a>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  )
+}
