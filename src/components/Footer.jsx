cebbb4992d438916b44c66b80defcca7a11a1792
Precious Affiah <74688829+preciousaffiah@users.noreply.github.com>
2023-01-29 18:55:24 -0600

    updated hover colour on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -125,7 +125,7 @@ export default function Example() {
                 <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
                     <div className="flex justify-center space-x-6 md:order-2">
                         {navigation.map((item) => (
-                            <a key={item.name} href={item.href} className="hover:text-gray-500">
+                            <a key={item.name} href={item.href} className="hover:text-black">
                                 <span className="sr-only">{item.name}</span>
                                 <item.icon className="h-6 w-6" aria-hidden="true" />
                             </a>
